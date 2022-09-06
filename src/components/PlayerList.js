c25d1119e8768745704d86ca413969364adb8bbc
scaprisecca <50277797+scaprisecca@users.noreply.github.com>
2022-09-05 18:33:07 -0700

    add search bar with filtering feature to the PlayerList component

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,37 +2,96 @@ import Players from '../data/players.json';
 import styles from './PlayerList.module.css';
 import { useEffect, useState } from 'react';
 
-const PlayerList = ({players, myTeam, draftPlayer, playerTaken}) => {
-    
-    return ( 
-        <table className={styles.table}>
-            <thead>
-                <tr>
-                    <th className={styles.tableRow}>Drafted?</th>
-                    <th className={styles.tableRow}>Position Rank</th>
-                    <th className={styles.tableRow}>Name - Team</th>
-                    <th className={styles.tableRow}>Position</th>
-                    <th className={styles.tableRow}>Projected Points</th>
-                    <th className={styles.tableRow}>ADP</th>
-                </tr>
-            </thead>
-            <tbody>
-                { players.map((player) => (
-                    <tr key={player.id}>
-                        <td className={styles.tableRow}>
-                            <img className={styles.tableIcon} src="../../img/draft-checkmark.svg" alt="Draft Player" onClick={() => draftPlayer(player.id, 'drafted')}/>
-                            <img className={styles.tableIcon} src="../../img/mark-drafted-x.svg" alt="Draft Player" onClick={() => playerTaken(player.id, 'taken')}/>
-                        </td>
-                        <td className={styles.tableRow}>{player.rank}</td>
-                        <td className={styles.tableRow}>{player.name} - {player.team}</td>
-                        <td className={styles.tableRow}>{player.position}</td>
-                        <td className={styles.tableRow}>{player.projectedPoints}</td>
-                        <td className={styles.tableRow}>{player.adp}</td>
-                    </tr>
-                ))}
-            </tbody>
+const PlayerList = ({players, draftPlayer, playerTaken}) => {
+    const [searchTerm, setSearchTerm] = useState('');
 
-        </table>
+    return ( 
+        <div>
+            {/* Search for players */}
+            <input
+                className={styles.searchBar}
+                type="text" 
+                placeholder="Search for Name, Position, or Team" 
+                onChange={event => setSearchTerm(event.target.value)} 
+            />
+            
+            {/* Table filters to only show players being searched */}
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th className={styles.tableRow}>Drafted?</th>
+                        <th className={styles.tableRow}>Position Rank</th>
+                        <th className={styles.tableRow}>Name - Team</th>
+                        <th className={styles.tableRow}>Position</th>
+                        <th className={styles.tableRow}>Projected Points</th>
+                        <th className={styles.tableRow}>ADP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {players.filter((search) => {
+                        if (searchTerm == '') {
+                            return search;
+                        } else if (search.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                            return search;
+                        }
+                    }).map((player, id) => (
+                        <tr key={player.id}>
+                            <td className={styles.tableRow}>
+                                <img className={styles.tableIcon} src="../../img/draft-checkmark.svg" alt="Draft Player" onClick={() => draftPlayer(player.id, 'drafted')}/>
+                                <img className={styles.tableIcon} src="../../img/mark-drafted-x.svg" alt="Draft Player" onClick={() => playerTaken(player.id, 'taken')}/>
+                            </td>
+                            <td className={styles.tableRow}>{player.rank}</td>
+                            <td className={styles.tableRow}>{player.name} - {player.team}</td>
+                            <td className={styles.tableRow}>{player.position}</td>
+                            <td className={styles.tableRow}>{player.projectedPoints}</td>
+                            <td className={styles.tableRow}>{player.adp}</td>
+                        </tr>
+                    ))}
+
+                    {/* { players.map((player) => (
+                        <tr key={player.id}>
+                            <td className={styles.tableRow}>
+                                <img className={styles.tableIcon} src="../../img/draft-checkmark.svg" alt="Draft Player" onClick={() => draftPlayer(player.id, 'drafted')}/>
+                                <img className={styles.tableIcon} src="../../img/mark-drafted-x.svg" alt="Draft Player" onClick={() => playerTaken(player.id, 'taken')}/>
+                            </td>
+                            <td className={styles.tableRow}>{player.rank}</td>
+                            <td className={styles.tableRow}>{player.name} - {player.team}</td>
+                            <td className={styles.tableRow}>{player.position}</td>
+                            <td className={styles.tableRow}>{player.projectedPoints}</td>
+                            <td className={styles.tableRow}>{player.adp}</td>
+                        </tr>
+                    ))} */}
+                </tbody>
+            </table>
+        </div>
+
+        // <table className={styles.table}>
+        //     <thead>
+        //         <tr>
+        //             <th className={styles.tableRow}>Drafted?</th>
+        //             <th className={styles.tableRow}>Position Rank</th>
+        //             <th className={styles.tableRow}>Name - Team</th>
+        //             <th className={styles.tableRow}>Position</th>
+        //             <th className={styles.tableRow}>Projected Points</th>
+        //             <th className={styles.tableRow}>ADP</th>
+        //         </tr>
+        //     </thead>
+        //     <tbody>
+        //         { players.map((player) => (
+        //             <tr key={player.id}>
+        //                 <td className={styles.tableRow}>
+        //                     <img className={styles.tableIcon} src="../../img/draft-checkmark.svg" alt="Draft Player" onClick={() => draftPlayer(player.id, 'drafted')}/>
+        //                     <img className={styles.tableIcon} src="../../img/mark-drafted-x.svg" alt="Draft Player" onClick={() => playerTaken(player.id, 'taken')}/>
+        //                 </td>
+        //                 <td className={styles.tableRow}>{player.rank}</td>
+        //                 <td className={styles.tableRow}>{player.name} - {player.team}</td>
+        //                 <td className={styles.tableRow}>{player.position}</td>
+        //                 <td className={styles.tableRow}>{player.projectedPoints}</td>
+        //                 <td className={styles.tableRow}>{player.adp}</td>
+        //             </tr>
+        //         ))}
+        //     </tbody>
+        // </table>
 
      );
 }
