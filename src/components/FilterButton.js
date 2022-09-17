3c77e1176dc1bca5ad258dd395101dc120b31409
scaprisecca <50277797+scaprisecca@users.noreply.github.com>
2022-09-17 11:40:12 -0700

    added styling to dropdown filters

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../App.css'
 
 const FilterButton = ({data, filterBy}) => {
@@ -15,9 +14,10 @@ const FilterButton = ({data, filterBy}) => {
     },[]);
 
     return ( 
-        <select name="" id="">
+        <select name="filterPlayers" className='filterDropdown'>
+            <option className='filterOption' value="All">All</option>
             {filter.map((item, index)=> (
-                <option key={index} value={item}>{item}</option>
+                <option className='filterOption' key={index} value={item}>{item}</option>
             ))}
         </select>
      );
